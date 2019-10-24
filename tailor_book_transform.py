0e3ae794b568a8300f2b2d1da4164e8a199fb185
Luca Baffa <luca@openbookpublishers.com>
2019-10-24 17:07:46 +0100

    simplify tailor_book_transform.py to work in the same folder as the xslt files

--- tailor_book_transform.py
+++ tailor_book_transform.py
@@ -1,24 +1,13 @@
 from bs4 import BeautifulSoup
 import os
 
-in_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                       'XML-last',
-                       'Transform-to-XML-book.xsl')
-out_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                       'XML-last',
-                        'Transform-to-XML-book_tailored.xsl')
-
-with open(in_path, 'r') as input:
+with open('Transform-to-XML-book.xsl', 'r') as input:
     soup = BeautifulSoup(input, 'html.parser')
 
     includes = soup.find_all('xi:include')
     for include in includes:
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                 'XML-last',
-                                 include.get('href'))
-
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(include.get('href')):
             include.extract()
 
-    with open(out_path, 'w') as output:
+    with open('Transform-to-XML-book_tailored.xsl', 'w') as output:
         output.write(soup.prettify(formatter='minimal'))
